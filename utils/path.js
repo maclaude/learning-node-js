b9e5e7406f31f8b5384382a08d3a583e2bf1da9b
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-04-22 17:26:54 +0200

    Rename server.js to app.js

--- utils/path.js
+++ utils/path.js
@@ -6,5 +6,5 @@ const path = require('path');
 /**
  * Export
  */
-// mainModule = Module where we start our application `server.js`
+// mainModule = Module where we start our application `app.js`
 module.exports = path.dirname(process.mainModule.filename);
