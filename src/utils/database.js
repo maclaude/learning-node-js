614684780352fe7631d093fd5f24502be1f66606
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-04-29 14:33:21 +0200

    Connecting Sequelize to the database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,23 +1,22 @@
 /**
  * NPM import
  */
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
 /**
  * Connection
  */
-// Connection pools help reduce the time spent connecting to the MySQL server
-// by reusing a previous connection, leaving them open
-// instead of closing when you are done with them.
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: 'localhost',
+  },
+);
 
 /**
- * Export
- */
-// Promise in order to get then & catch
-module.exports = pool.promise();
+* Export
+*/
+module.exports = sequelize;
