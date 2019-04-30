ad6b50ced66459d3a544680b0ebb5a3fb301a435
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-04-30 12:23:56 +0200

    Adding new products to the cart

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-shadow */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 // Products Controller
@@ -68,10 +70,31 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const { productId } = req.body;
-  Product.findById(productId, (product) => {
-    Cart.addProduct(productId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      const newQuantity = 1;
+      if (product) {
+        // ...
+      }
+      return Product.findByPk(productId)
+        .then(product => fetchedCart.addProduct(product, {
+          through: { quantity: newQuantity },
+        }))
+        .catch(err => console.log(err));
+    })
+    .then(() => res.redirect('/cart'))
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
