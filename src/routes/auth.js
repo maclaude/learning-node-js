59d619dcc42743cb22ea968fa1311eb08835db3c
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-05-11 11:53:56 +0200

    Built-in & custom validators

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -38,7 +38,15 @@ router.post(
   '/signup',
   check('email')
     .isEmail()
-    .withMessage('Please enter a valid email'),
+    .withMessage('Please enter a valid email')
+    // eslint-disable-next-line no-unused-vars
+    .custom((value, { req }) => {
+      if (value === 'test@test.com') {
+        throw new Error('This email address is forbidden !');
+      }
+      // Important to return true after the condition
+      return true;
+    }),
   postSignup
 );
 
