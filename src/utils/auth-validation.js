a9829da8c69f9b104cd33cf89e4e45dce48c1703
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-05-13 16:38:14 +0200

    Sanitizing data

--- src/utils/auth-validation.js
+++ src/utils/auth-validation.js
@@ -26,27 +26,29 @@ const signupValidation = [
         }
         return true;
       });
+    })
+    .normalizeEmail(),
+  body('password', 'Please enter a password with at least 8 characters')
+    .isLength({ min: 8 })
+    .trim(),
+  body('confirmPassword')
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password and confirmed password aren't the same");
+      }
+      return true;
     }),
-  body(
-    'password',
-    'Please enter a password with at least 8 characters'
-  ).isLength({ min: 8 }),
-  body('confirmPassword').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Password and confirmed password aren't the same");
-    }
-    return true;
-  }),
 ];
 
 const loginValidation = [
   check('email')
     .isEmail()
-    .withMessage('Please enter a valid email'),
-  body(
-    'password',
-    'Please enter a password with at least 8 characters'
-  ).isLength({ min: 8 }),
+    .withMessage('Please enter a valid email')
+    .normalizeEmail(),
+  body('password', 'Please enter a password with at least 8 characters')
+    .isLength({ min: 8 })
+    .trim(),
 ];
 
 /**
